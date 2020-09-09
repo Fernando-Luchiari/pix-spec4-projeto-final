c3457583fc7e0ab60feeffc1cc62b9f6ead110e4
Fernando Luchiari Duarte da Silva <luchiari.fernando@gmail.com>
2020-09-09 11:09:03 -0300

    MS structure

---
+++ bond/src/main/java/br/com/mastertech/bond/BondApplication.java
@@ -0,0 +1,13 @@
+package br.com.mastertech.bond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondApplication.class, args);
+	}
+
+}
